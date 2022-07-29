6af906e8881fd5130df80207d583c706eab528c6
kuizuo <kuizuo12@163.com>
2022-07-29 22:44:46 +0800

    feat: init

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <nav text-xl mt-6>
+    <RouterLink class="icon-btn mx-2" to="/" title="首页">
+      <div i-carbon-campsite />
+    </RouterLink>
+
+    <button class="icon-btn mx-2 !outline-none" @click="toggleDark()">
+      <div i="carbon-sun dark:carbon-moon" />
+    </button>
+
+    <RouterLink class="icon-btn mx-2" to="/about" title="关于">
+      <div i-carbon-dicom-overlay />
+    </RouterLink>
+
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/kuizuo/example" target="_blank" title="GitHub">
+      <div i-carbon-logo-github />
+    </a>
+  </nav>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div />
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
