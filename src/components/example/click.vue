05246a58bd2be07f7546a95e9ae67eb1a901566d
kuizuo <kuizuo12@163.com>
2022-08-09 06:45:47 +0800

    fix: vitessr build error "document is not defined"

--- src/components/example/click.vue
+++ src/components/example/click.vue
@@ -1,6 +1,157 @@
 <script lang="ts" setup>
-import cursorSpecialEffects from '~/assets/js/firework.js'
 import { words } from '~/assets/js/cet4.js'
+
+const createFirework = (function () {
+  class Circle {
+    constructor({ origin, speed, color, angle, context }) {
+      this.origin = origin
+      this.position = { ...this.origin }
+      this.color = color
+      this.speed = speed
+      this.angle = angle
+      this.context = context
+      this.renderCount = 0
+    }
+
+    draw() {
+      this.context.fillStyle = this.color
+      this.context.beginPath()
+      this.context.arc(this.position.x, this.position.y, 2, 0, Math.PI * 2)
+      this.context.fill()
+    }
+
+    move() {
+      this.position.x = (Math.sin(this.angle) * this.speed) + this.position.x
+      this.position.y = (Math.cos(this.angle) * this.speed) + this.position.y + (this.renderCount * 0.3)
+      this.renderCount++
+    }
+  }
+
+  class Boom {
+    constructor({ origin, context, circleCount = 10, area }) {
+      this.origin = origin
+      this.context = context
+      this.circleCount = circleCount
+      this.area = area
+      this.stop = false
+      this.circles = []
+    }
+
+    randomArray(range) {
+      const length = range.length
+      const randomIndex = Math.floor(length * Math.random())
+      return range[randomIndex]
+    }
+
+    randomColor() {
+      const range = ['8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+      return `#${this.randomArray(range)}${this.randomArray(range)}${this.randomArray(range)}${this.randomArray(range)}${this.randomArray(range)}${this.randomArray(range)}`
+    }
+
+    randomRange(start, end) {
+      return (end - start) * Math.random() + start
+    }
+
+    init() {
+      for (let i = 0; i < this.circleCount; i++) {
+        const circle = new Circle({
+          context: this.context,
+          origin: this.origin,
+          color: this.randomColor(),
+          angle: this.randomRange(Math.PI - 1, Math.PI + 1),
+          speed: this.randomRange(1, 6),
+        })
+        this.circles.push(circle)
+      }
+    }
+
+    move() {
+      this.circles.forEach((circle, index) => {
+        if (circle.position.x > this.area.width || circle.position.y > this.area.height)
+          return this.circles.splice(index, 1)
+
+        circle.move()
+      })
+      if (this.circles.length === 0)
+        this.stop = true
+    }
+
+    draw() {
+      this.circles.forEach(circle => circle.draw())
+    }
+  }
+
+  class Firework {
+    constructor() {
+      this.computerCanvas = document.createElement('canvas')
+      this.renderCanvas = document.createElement('canvas')
+
+      this.computerContext = this.computerCanvas.getContext('2d')
+      this.renderContext = this.renderCanvas.getContext('2d')
+
+      this.globalWidth = window.innerWidth
+      this.globalHeight = window.innerHeight
+
+      this.booms = []
+      this.running = false
+    }
+
+    createFirework(e) {
+      const boom = new Boom({
+        origin: { x: e.clientX, y: e.clientY },
+        context: this.computerContext,
+        area: {
+          width: this.globalWidth,
+          height: this.globalHeight,
+        },
+      })
+      boom.init()
+      this.booms.push(boom)
+      this.running || this.run()
+    }
+
+    init() {
+      const style = this.renderCanvas.style
+      style.position = 'fixed'
+      style.top = style.left = 0
+      style.zIndex = '9999'
+      style.pointerEvents = 'none'
+
+      style.width = this.renderCanvas.width = this.computerCanvas.width = this.globalWidth
+      style.height = this.renderCanvas.height = this.computerCanvas.height = this.globalHeight
+
+      document.body.append(this.renderCanvas)
+    }
+
+    run() {
+      this.running = true
+      if (this.booms.length === 0)
+        return this.running = false
+
+      requestAnimationFrame(this.run.bind(this))
+
+      this.computerContext.clearRect(0, 0, this.globalWidth, this.globalHeight)
+      this.renderContext.clearRect(0, 0, this.globalWidth, this.globalHeight)
+
+      this.booms.forEach((boom, index) => {
+        if (boom.stop)
+          return this.booms.splice(index, 1)
+
+        boom.move()
+        boom.draw()
+      })
+      this.renderContext.drawImage(this.computerCanvas, 0, 0, this.globalWidth, this.globalHeight)
+    }
+  }
+
+  const firework = new Firework()
+  firework.init()
+
+  return function (e) {
+    return firework.createFirework(e)
+  }
+})()
+
 const type = $ref('love')
 
 onMounted(() => {
@@ -12,7 +163,7 @@ onMounted(() => {
     else if (type === 'social')
       createSocial(e)
     else if (type === 'firework')
-      cursorSpecialEffects.createFirework(e)
+      createFirework(e)
   }
 })
 
