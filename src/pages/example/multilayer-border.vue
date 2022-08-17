43c60dcb6d87c38ed6deb31e28052656881b5cc6
kuizuo <kuizuo12@163.com>
2022-08-17 08:34:34 +0800

    feat: add some example

---
+++ src/pages/example/multilayer-border.vue
@@ -0,0 +1,22 @@
+<template>
+  <div flex justify-center items-center>
+    <div class="box" />
+  </div>
+</template>
+
+<style scoped>
+.box {
+  margin: 2rem;
+  width: 200px;
+  height: 200px;
+  background: #fffeec;
+  box-shadow: 0 0 0 10px #9bf4d5, 0 0 0 20px #1dad9b, 0 0 0 30px #346357;
+}
+</style>
+
+<route lang="yaml">
+name: 多重边框
+meta:
+  layout: demo
+</route>
+
---
+++ src/pages/example/neumorphism.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="p-4 flex justify-center items-center gap-4 bg-[#ececec]">
+    <div class="input-box">
+      <input class="input-check" type="checkbox">
+      <i class="icon i-carbon-power" />
+    </div>
+    <div class="logo-box">
+      <i class="icon i-carbon-carbon" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input-box {
+  position: relative;
+  width: 100px;
+  height: 100px;
+}
+
+.input-check {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 50%;
+  box-shadow: -10px -10px 15px rgba(255, 255, 255, 0.5),
+    10px 10px 15px rgba(70, 70, 70, 0.12);
+  border: 8px solid #ececec;
+  appearance: none;
+  cursor: pointer;
+  z-index: 1;
+}
+
+.input-check:checked {
+  box-shadow: -10px -10px 15px rgba(255, 255, 255, 0.5),
+    10px 10px 15px rgba(70, 70, 70, 0.12),
+    inset -10px -10px 15px rgba(255, 255, 255, 0.5),
+    inset 10px 10px 15px rgba(70, 70, 70, 0.12);
+}
+
+.input-box .icon {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: rgba(55, 65, 81);
+}
+
+.input-check:checked+.input-icon {
+  color: #15e38a;
+}
+
+.logo-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+  background-color: #ececec;
+  box-shadow: -10px -10px 15px rgba(255, 255, 255, 0.5),
+    10px 10px 15px rgba(70, 70, 70, 0.12);
+  padding: 2rem;
+  cursor: pointer;
+  transform: box-shadow .2s ease-out;
+}
+
+.logo-box:hover {
+  box-shadow: 0 0 15px rgba(255, 255, 255, 0.5),
+    0 0 15px rgba(70, 70, 70, 0.12),
+    inset -10px -10px 15px rgba(255, 255, 255, 0.5),
+    inset 10px 10px 15px rgba(70, 70, 70, 0.12);
+  transition: box-shadow .2s ease-out;
+}
+
+.logo-box .icon {
+  width: 30px;
+  height: 30px;
+  color: rgba(55, 65, 81);
+}
+</style>
+
+<route lang="yaml">
+name: 新拟态新拟物风格
+meta:
+  layout: demo
+</route>
