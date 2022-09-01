c0cc87c2ef2e8bbc9d4ac2869946ef9a28f273be
kuizuo <kuizuo12@163.com>
2022-09-01 18:19:07 +0800

    feat: add congratulations.vue

---
+++ src/pages/example/congratulations.vue
@@ -0,0 +1,35 @@
+<script setup>
+import JSConfetti from 'js-confetti'
+
+const confetti = new JSConfetti()
+
+function showConfetti() {
+  confetti.addConfetti()
+}
+
+showConfetti()
+</script>
+
+<template>
+  <div cursor-pointer @click="showConfetti">
+    🎉 Congratulations!
+  </div>
+  <div mt-4>
+    source: <a href="https://github.com/loonywizard/js-confetti" target="_blank" text-cyan>js-confetti</a>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+}
+</style>
+
+<route lang="yaml">
+name: 五彩纸屑
+meta:
+  layout: demo
+</route>
+
